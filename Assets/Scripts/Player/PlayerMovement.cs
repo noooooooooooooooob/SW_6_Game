cbca14f6d8e8784937071fe89d1083f2f3fd7e61
John Sim <johnnygamesover9000@gmail.com>
2024-10-04 22:53:57 +0900

    Added player-mouse tracking

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float jump_time_to_descent;
     public float crouchSize;
 
+    public Transform body;
 
 
     private float jump_velocity;
@@ -24,9 +25,11 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        body = transform.Find("BodyPivot");
         jump_velocity = 2 * jumpHeight / jump_time_to_peak;
         jump_gravity = 2 * jumpHeight / (jump_time_to_peak * jump_time_to_peak);
         fall_gravity = 2 * jumpHeight / (jump_time_to_descent * jump_time_to_descent);
+
     }
 
     // Update is called once per frame
@@ -49,23 +52,29 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = false;
         }
 
-        Debug.Log(Input.GetAxis("Horizontal"));
-        if (moveX > 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        if (moveX < 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-        }
+        // Debug.Log(Input.GetAxis("Horizontal"));
+        // if (moveX > 0)
+        // {
+        //     transform.rotation = Quaternion.Euler(0, 0, 0);
+        // }
+        // if (moveX < 0)
+        // {
+        //     transform.rotation = Quaternion.Euler(0, 180, 0);
+        // }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            transform.localScale = new Vector2(transform.localScale.x, crouchSize * transform.localScale.y);
+            if (body != null)
+            {
+                body.localScale = new Vector2(body.localScale.x, crouchSize * body.localScale.y);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
         {
-            transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y / crouchSize);
+            if (body != null)
+            {
+                body.localScale = new Vector2(body.localScale.x, body.localScale.y / crouchSize);
+            }
         }
     }
 
