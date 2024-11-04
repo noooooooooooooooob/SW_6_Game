70b15196aa7903b0afa2a00348981bc4e8665e9f
John Sim <johnnygamesover9000@gmail.com>
2024-11-04 16:29:03 +0900

    Added missing prefabs

--- Assets/Scripts/item/inventory.cs
+++ Assets/Scripts/item/inventory.cs
@@ -6,9 +6,9 @@ public class inventory : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] itemsMini;
-   private List<string> inventorys = new List<string>();
+    private List<string> inventorys = new List<string>();
     private int inventorySize = 3;
-    private float[] arrPosX = {-3.8f,-3f,-2.2f};
+    private float[] arrPosX = { -3.8f, -3f, -2.2f };
     private List<GameObject> spawnedItems = new List<GameObject>();
 
     void Start()
@@ -23,7 +23,7 @@ public class inventory : MonoBehaviour
 
     void Update()
     {
-    
+
 
         // 각 숫자 키로 아이템 사용
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -50,18 +50,19 @@ public class inventory : MonoBehaviour
                 inventorys[i] = itemtype;
                 Debug.Log($"Slot {i + 1}에 {itemtype}이(가) 추가되었습니다.");
                 int number;
-                 int.TryParse(itemtype, out number);
-                spawnitem(number-1,arrPosX[i],i);
+                int.TryParse(itemtype, out number);
+                spawnitem(number - 1, arrPosX[i], i);
                 return;
             }
         }
         Debug.Log("인벤토리가 가득 찼습니다.");
     }
-    void spawnitem(int index,float posX,int slotIndex){
-        Vector3 spawnPos = new Vector3(posX,transform.position.y,transform.position.z-1f);
-        
+    void spawnitem(int index, float posX, int slotIndex)
+    {
+        Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z - 1f);
+
         GameObject newItem = Instantiate(itemsMini[index], spawnPos, Quaternion.identity);
-        spawnedItems[slotIndex] = newItem; 
+        spawnedItems[slotIndex] = newItem;
     }
     // 지정된 슬롯의 아이템 사용
     private void UseItem(int slotIndex)
@@ -75,7 +76,7 @@ public class inventory : MonoBehaviour
         if (inventorys[slotIndex] != null)
         {
             Debug.Log($"Slot {slotIndex + 1}의 {inventorys[slotIndex]}을(를) 사용합니다.");
-             if (spawnedItems[slotIndex] != null)
+            if (spawnedItems[slotIndex] != null)
             {
                 Destroy(spawnedItems[slotIndex]);
                 spawnedItems[slotIndex] = null; // Clear the reference
