bfbca8ccbee99ccd4056d4bef1402119c4fcbe09
John Sim <johnnygamesover9000@gmail.com>
2024-11-05 21:00:27 +0900

    Added Time.deltaTime

--- Assets/Scripts/item/item.cs
+++ Assets/Scripts/item/item.cs
@@ -14,37 +14,38 @@ public class item : MonoBehaviour
 
     [SerializeField]
     private inventory inventorys;
-    
+
     void Start()
     {
-         // inventorys가 설정되지 않았다면 자동으로 할당해줌 (필요 시)
+        // inventorys가 설정되지 않았다면 자동으로 할당해줌 (필요 시)
         if (inventorys == null)
         {
             inventorys = GameObject.FindObjectOfType<inventory>();
         }
     }
-  
-    
+
+
     // Update is called once per frame
     void Update()
     {
-         transform.Translate(-0.1f*speed , 0, 0); // 등속 왼쪽 이동
+        transform.Translate(-1f * speed * Time.deltaTime, 0, 0); // 등속 왼쪽 이동
     }
-    void OnTriggerEnter2D(Collider2D collision) {
-         if (collision.gameObject.CompareTag("Ground"))
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject); // 바닥에 닿으면 삭제
         }
         else if (collision.gameObject.CompareTag("Player"))
         {
-            
+
             if (inventorys != null)
             {
-                
+
                 inventorys.AddItem(itemType); // 아이템 종류를 인벤토리에 추가
             }
             Destroy(gameObject); // 인벤토리에 추가 후 삭제
         }
     }
-    
+
 }
