a5d3a4d5e003b607b7134172791a1f2869e9124d
noooooooooooooooob <pisas@naver.com>
2024-11-12 21:26:08 +0900

    Sprite Added / Boss Logic Added
    플레이어 공격 다양화
    기믹들 함수 구현
     - > 노트 스프라이트를 변화함으로써
          무슨 기믹인지 파악할 수있도록
          하는 것이 중요할듯

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Note : MonoBehaviour
@@ -10,30 +11,47 @@ public class Note : MonoBehaviour
     public float[] spawnPoints;
     public Sprite[] sprites;
     SpriteRenderer spriteRenderer;
+    int spawnPointYidx;
     float spawnPointY;
     public float spawnPointX;
     public float speed;
     bool isBoardered;
     public bool isHit;
-    public float prefabIdx; // Object Manager 안에서 배열 인덱스
+    int arrowidx;
 
     public bool isLeft;
     public bool isRight;
     public bool isUp;
     public bool isDown;
 
+    //기믹들 추가
+    public bool isChange;
+    public float changeSpeed;
+    public bool isOpposite;
+    public bool isNotacted;
+    public bool isFaded;
+    public float op;
+
+
     // 체력 변화 처리 클래스 연결
     public HealthBarController healthBarController;
 
     void Awake()
     {
+        op=1.0f;
         rigid=GetComponent<Rigidbody2D>();
         spriteRenderer=GetComponent<SpriteRenderer>();
-        spawnPointY=spawnPoints[Random.Range(0,3)]; // 스폰포인트 랜덤
+        spawnPointYidx=Random.Range(0,3);
+        spawnPointY=spawnPoints[spawnPointYidx]; // 스폰포인트 랜덤
         transform.position=new Vector2(spawnPointX,spawnPointY);
 
+        isChange=false;
+        isOpposite=false;
+        isNotacted=false;
+        isFaded=false;
+
         int coloridx=Random.Range(0,3);
-        int arrowidx=Random.Range(0,4);
+        arrowidx=Random.Range(0,4);
 
         isLeft=false;
         isRight=false;
@@ -87,31 +105,87 @@ public class Note : MonoBehaviour
 
     void Update()
     {
-        if(!isHit)
-            transform.Translate(-1.0f * speed*Time.deltaTime,0,0); // 등속으로 왼쪽으로 이동
-        else
+        if(isChange)
         {
-            
+            locateChange();
+            isChange=false;
         }
+        if(isFaded)
+        {
+            isFaded=false;
+            fadeNodes();
+        }
+        if(isOpposite)
+        {
+            switch(arrowidx)
+            {
+                case 0:
+                    isLeft=false;
+                    isRight=true;
+                    break;
+                case 1:
+                    isRight=false;
+                    isLeft=true;
+                    break;
+                case 2:
+                    isUp=false;
+                    isDown=true;
+                    break;
+                case 3:
+                    isDown=false;
+                    isUp=true;
+                    break;
+            }
+            isOpposite=false;
+        }
+        transform.Translate(-1.0f * speed*Time.deltaTime,0,0); // 등속으로 왼쪽으로 이동
 
     }
 
+    void locateChange()
+    {
+        spawnPointYidx++;
+        if(spawnPointYidx>2)
+            spawnPointYidx=0;
+        transform.position = new Vector2(transform.position.x, spawnPoints[spawnPointYidx]);
+        Invoke("locateChange",changeSpeed);
+    }
+
+    void fadeNodes()
+    {
+        spriteRenderer.color=new Color(1,1,1,1.0f*op);
+        if(op<=0.0f)
+            return;
+        op-=0.3f;
+        Invoke("fadeNodes",1.0f);
+    }
 
     void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.tag=="Ground")
         {
+            HealthBarController healthBarController = FindObjectOfType<HealthBarController>();
+            if(healthBarController!=null)
+            {
+                HealthBarController.Instance.TakeDamage();
+            }
+            else
+            {
+                Debug.LogError("HealthBarController not found on Player object.");
+            }
             Destroy(gameObject);
         }
         else if(collision.gameObject.tag=="Player")
         {
             HealthBarController healthBarController = FindObjectOfType<HealthBarController>();
-            if(healthBarController!=null){
-            HealthBarController.Instance.TakeDamage();}
-            else{
+            if(healthBarController!=null)
+            {
+                HealthBarController.Instance.TakeDamage();
+            }
+            else
+            {
                 Debug.LogError("HealthBarController not found on Player object.");
             }
             Destroy(gameObject);
-            
         }
     }
 }
