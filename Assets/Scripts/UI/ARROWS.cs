a5588646e3e60237e659c9d6694fff37246dbb29
noooooooooooooooob <pisas@naver.com>
2024-11-21 15:44:58 +0900

    Main Menu uploaded

---
+++ Assets/Scripts/UI/ARROWS.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARROWS : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
